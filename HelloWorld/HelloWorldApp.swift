c8479b3785f0e0b44a11efd4d240606d25715e2b
paul1464403 <158220392+paul1464403@users.noreply.github.com>
2024-02-13 16:44:55 -0800

    Add files via upload

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by paul on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
